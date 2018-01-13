9264cd416065066143708b5c984e43b83cca7c4e
FreakingHulk <34045304+FreakingHulk@users.noreply.github.com>
2018-01-13 16:20:29 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const discord = require('discord.js')
+const bot = new discord.Client()
+
+bot.on("ready", () => {
+   bot.user.setGame("HENLO")
+})
+
+bot.on("message", message => {
+  if (message.content == "henlo") {
+    message.channel.send("henlo"
+  }
+})
